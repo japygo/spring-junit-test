aeb5af67bff6342ae1bf9814b73467da6e44cb7c
japygo <hyunnjinn@gmail.com>
2022-12-27 23:27:40 +0900

    init project

---
+++ src/main/java/com/example/junit/SpringJunitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJunitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJunitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/junit/AssumptionsTest.java
@@ -0,0 +1,38 @@
+package com.example.junit;
+
+import com.example.junit.util.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+public class AssumptionsTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void testOnlyOnCiServer() {
+        assumeTrue("CI".equals(System.getenv("ENV")));
+        // remainder of test
+    }
+
+    @Test
+    void testOnlyOnDeveloperWorkstation() {
+        assumeTrue("DEV".equals(System.getenv("ENV")),
+                () -> "Aborting test: not on developer workstation");
+        // remainder of test
+    }
+
+    @Test
+    void testInAllEnvironments() {
+        assumingThat("CI".equals(System.getenv("ENV")),
+                () -> {
+                    // perform these assertions only on the CI server
+                    assertEquals(2, calculator.divide(4, 2));
+                });
+
+        // perform these assertions in all environments
+        assertEquals(42, calculator.multiply(6, 7));
+    }
+}
